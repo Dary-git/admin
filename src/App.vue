c094fb2213fc6c3eb44cad9064cc8a1afe54da51
Dary <1142639600@qq.com>
2022-11-06 21:03:21 +0800

    init vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="vue">vue</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+    margin:0;
+    padding:0;
+}
+</style>
